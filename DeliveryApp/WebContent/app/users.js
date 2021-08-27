2339c8e0b4912ef6ce5b855cacee2d3b72f9f92c
lukamatic <matic.ra16.2018@uns.ac.rs>
2021-08-27 11:10:55 +0200

    Improved users display. Edited database

--- DeliveryApp/WebContent/app/users.js
+++ DeliveryApp/WebContent/app/users.js
@@ -10,54 +10,64 @@ Vue.component('users', {
     };
   },
   template: ` 
-	<div>
-		<navbar path="users"></navbar>
-	    <div class="d-flex flex-column align-items-center pb-5 bg-light">
-	      <div
-	        v-for="user in displayedUsers"
-	        :key="user.id"
-	        class="
-	          d-flex
-	          flex-row
-	          align-items-center
-	          flex-wrap flex-md-nowrap
-	          justify-content-center
-	          w-75
-	          mt-4
-	          bg-white
-	          border border-dark
-	        "
-	      >
-	        <img class="m-3" src="" width="100" height="100" />
+	
+  <div>
+    <div class="d-flex flex-column align-items-center pb-5 bg-light">
+      <div
+        v-for="user in displayedUsers"
+        :key="user.id"
+        class="
+          d-flex
+          flex-row
+          align-items-center
+          flex-wrap flex-md-nowrap
+          justify-content-center
+          w-75
+          mt-4
+          bg-white
+          border border-dark
+        "
+      >
         <div class="flex-column flex-fill mx-3">
           <h5 class="mt-2">Username: {{ user.username }}</h5>
           <h5>Name: {{ user.firstName }} {{ user.lastName }}</h5>
           <h5>Role: {{ user.role }}</h5>
+          <h5>Date of birth: {{ user.dateOfBirth | formatDate('DD.MM.YYYY.') }}</h5>
+          <h5>Gender: {{ user.gender }}</h5>
           <h5 v-if="user.role == 'CUSTOMER'">
             Customer type: {{ user.customerType.customerTypeName }}
           </h5>
           <h5 v-if="user.role == 'CUSTOMER'">Points: {{ user.points }}</h5>
         </div>
         <div v-if="user.role != 'ADMIN'" class="flex-column">
+          <div v-if="user.suspicious">
+            <button
+              class="btn btn-danger font-weight-bold mt-3 mr-3"
+              style="width: 120px; pointer-events: none"
+            >
+              Suspicious
+            </button>
+          </div>
           <div>
             <button
-              class="btn btn-dark font-weight-bold m-2 mr-3"
-              style="width: 80px"
+              class="btn btn-dark font-weight-bold my-2 mr-3"
+              style="width: 120px"
             >
               Block
             </button>
           </div>
           <div>
             <button
-              class="btn btn-dark font-weight-bold m-2 mr-3"
-              style="width: 80px"
+              class="btn btn-dark font-weight-bold mb-2 mr-3"
+              style="width: 120px"
             >
               Delete
             </button>
           </div>
-	      </div>
-	    </div>
-    </div>	  
+        </div>
+      </div>
+    </div>
+  </div>
 `,
   mounted() {
     axios.get("/DeliveryApp/rest/users").then((response) => {
@@ -66,8 +76,9 @@ Vue.component('users', {
     });
   },
   filters: {
-    formatAddress: (address) => {
-      return address.street + ' ' + address.streetNumber + ', ' + address.city + ', ' + address.country;
+    formatDate: function (value, format) {
+      var parsed = moment(value);
+      return parsed.format(format);
     },
   },
 });
